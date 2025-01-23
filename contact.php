df217cdccdf3a3ae82ee694f485e8dc53d66d86a
Cameron Asselin <100627085+CameronAsselin@users.noreply.github.com>
2025-01-23 17:05:54 -0500

    Added contact.php

---
+++ contact.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Cameron Asselin - Contact</title>
+		<link rel="stylesheet" href="style.css">
+	</head>
+  <body>
+    <div id="outer" class="flex-container">
+		  <?php include 'header.php'; ?>		
+		  <?php include 'sidebar.php'; ?>
+		  <div id="main">
+        <h2>Contact</h2>
+        <br>
+        <h3>E-Mail</h3>
+        <p><a href="mailto:cameron.m.asselin@gmail.com">cameron.m.asselin@gmail.com</a></p>
+		  </div>
+      <?php include 'footer.php'; ?>
+    </div>
+	</body>
+</html> 
